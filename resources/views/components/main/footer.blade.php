10843aace8a2d6a0f4b9fa4cccad59e37bda2f55
xlFANTASYlx <tima29taipov@gmail.com>
2023-05-16 20:20:15 +0500

    Пожалуйста заработай!

--- resources/views/components/main/footer.blade.php
+++ resources/views/components/main/footer.blade.php
@@ -3,22 +3,22 @@
         <div class="footer_row">
             <div class="footer_about">
                 <div class="footer_title">О нас</div>
-                <div class="footer_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit</div>
+                <div class="footer_text">Мы — команда, которая создала этот сайт, чтобы помочь талантливым фотографам продемонстрировать свои работы во всей красе.</div>
             </div>
             <div class="footer_navigation">
                 <div class="footer_title">Навигация</div>
                 <ul class="footer_list">
-                    <li>Главная</li>
-                    <li>Log in</li>
-                    <li>Топ авторов</li>
+                    <li><a href="" class="main_link">Главная</a></li>
+                    <li><a href="" class="log_link">Log in</a></li>
+                    <li><a href="" class="rating_link">Топ авторов</a></li>
                 </ul>
             </div>
             <div class="footer_socials">
                 <div class="footer_title">Социальные сети</div>
                 <div class="socials_row">
-                    <img class="icon" src="{{asset('img/social/Google.png')}}" alt="">
-                    <img class="icon" src="{{asset('img/social/Apple.png')}}" alt="">
-                    <img class="icon" src="{{asset('img/social/VK.svg')}}" alt="">
+                    <a href="" class="google_link"><img class="footer_icon" src="{{asset('img/social/Google.png')}}" alt=""></a>
+                    <a href="" class="apple_link"><img class="footer_icon" src="{{asset('img/social/Apple.png')}}" alt=""></a>
+                    <a href="" class="vk_link"><img class="footer_icon" src="{{asset('img/social/VK.svg')}}" alt=""></a>
                 </div>
             </div>
         </div>
