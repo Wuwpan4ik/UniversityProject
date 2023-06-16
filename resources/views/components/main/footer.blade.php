75137e2b4c344192450e2a67b21fdd2d77df4ecd
Wuwpan4ik <dimalim110@gmail.com>
2023-06-16 22:35:25 +0500

    Сделал соц сети почту

--- resources/views/components/main/footer.blade.php
+++ resources/views/components/main/footer.blade.php
@@ -8,17 +8,15 @@
             <div class="footer_navigation">
                 <div class="footer_title">Навигация</div>
                 <ul class="footer_list">
-                    <li><a href="" class="main_link footer_text">Главная</a></li>
-                    <li><a href="" class="log_link footer_text">Log in</a></li>
-                    <li><a href="" class="rating_link footer_text">Топ авторов</a></li>
+                    <li><a href="{{ route('home') }}" class="main_link footer_text">Главная</a></li>
+                    <li><a href="{{ route('topAuthor') }}" class="rating_link footer_text">Топ авторов</a></li>
+                    <li><a href="{{ route('topWorks') }}" class="rating_link footer_text">Топ работ</a></li>
                 </ul>
             </div>
             <div class="footer_socials">
                 <div class="footer_title footer_title__socials">Социальные сети</div>
-                <div class="socials_row">
-                    <a href="" class="google_link"><img class="footer_icon" src="{{asset('img/social/Google.png')}}" alt=""></a>
-                    <a href="" class="apple_link"><img class="footer_icon" src="{{asset('img/social/Apple.png')}}" alt=""></a>
-                    <a href="" class="vk_link"><img class="footer_icon" src="{{asset('img/social/VK.svg')}}" alt=""></a>
+                <div class="socials_row" style="justify-content: flex-start; padding: 15px 0;">
+                    <a style="color: white; font-size: 24px;" href="mailto:dimalim110@gmail.com">Почта</a>
                 </div>
             </div>
         </div>
